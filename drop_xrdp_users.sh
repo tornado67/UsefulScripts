025e3ea188dbb8035f2a2deb212b02b60c616060
Alex <misterespia666@yandex.ru>
2017-12-27 18:22:09 +0300

    Update drop_xrdp_users.sh

--- drop_xrdp_users.sh
+++ drop_xrdp_users.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
     CIFS_FOLDERS=`mount | grep cifs | cut -d " " -f 1` # получаем cifs тома
-    pgrep -f "\.xsession$" | kill -9 # вылавливаем процессы которые занимают файл .xsession и безжалостно убиваем
+    
+    pgrep -f "\.xsession$"  | xargs kill  # вылавливаем процессы которые занимают файл .xsession и безжалостно убиваем
 
-    for cifs_folder in  $CIFS_FOLDERS; do
-        mount -f -o remount,rw  $cifs_folder 
+    for cifs_folder in  $CIFS_FOLDERS; do # перебираем шары 
+    
+        mount -f -o remount,rw  $cifs_folder #и принудительно перемонтируем
+
     done
