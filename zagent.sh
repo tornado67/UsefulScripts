78100127faf425ced92632e0276ab19226bf46e6
Alex <misterespia666@yandex.ru>
2018-06-22 00:02:06 +0300

    Create zagent.sh

---
+++ zagent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
+dpkg -i zabbix-release_3.4-1+stretch_all.deb
+apt update 
+apt-get install zabbix-agent -y
+sed -i.backup -e 's/#.*$//' -e '/^$/d' /etc/zabbix/zabbix_agentd.conf
