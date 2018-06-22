e3f281998eb1a2bd3da630811d9d4f8531d04ba9
Alex <misterespia666@yandex.ru>
2018-06-22 10:52:03 +0300

    Update zagent.sh

--- zagent.sh
+++ zagent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# просто для облегчения жизни
 wget http://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
 dpkg -i zabbix-release_3.4-1+stretch_all.deb
 apt update 
