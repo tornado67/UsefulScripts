24e320dad185c27219861bd0f2cf71f1824c6111
Alex <misterespia666@yandex.ru>
2018-07-20 16:25:40 +0300

    Update sort_squid_logs.sh

--- linux/sort_squid_logs.sh
+++ linux/sort_squid_logs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-SQUID_LOG=/var/log/squid/access.log
+DEFAULT_SQUID_LOG=/var/log/squid/access*
+SQUID_LOG=$1
 
-grep -i denie $SQUID_LOG | sed -n -e 's/^.*http*:\/\///p' | sed  -e 's/www\.//p' | cut -d '/' -f 1 | sort | uniq -c  | sort -nr
+if [ -z $SQUID_LOG]; then
+    SQUID_LOG=$DEFAULT_SQUID_LOG
+fi
+
+grep -Ri denie $SQUID_LOG | sed -n -e 's/^.*http*:\/\///p' | sed  -e 's/www\.//p' | cut -d '/' -f 1 | sort | uniq -c  | sort -nr | column -t -s' '
